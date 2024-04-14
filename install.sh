d5c2892f6d69723b112d4a92fc94eac29fc93698
a5ucanc <itaycheredman@gmail.com>
2024-04-14 13:43:56 +0300

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Build images and create containers
+docker compose up --build -d
+
+# Generate certificats for admin servers
+docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d gophish.live --register-unsafely-without-email
+docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d evil.gophish.live --register-unsafely-without-email
+
+# Activate https for admin servers
+docker compose exec nginx sed 's/#//g' nginx/conf.d/default.conf
+docker compose exec nginx nginx -s reload
