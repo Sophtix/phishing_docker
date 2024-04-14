52d39fe9ca078da880c43ae30a1d096716be3253
a5ucanc <itaycheredman@gmail.com>
2024-04-14 17:57:53 +0300

    remove port forwarding

--- add-domain.sh
+++ add-domain.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if running as root
-if [ "$EUID" -ne 0 ]; then
-    echo "This script must be run as root"
-    exit 1
-fi
-
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <domain>"
     exit 1
