cc548d6ed8e0c2cb9b93e26040edfa37fc6de183
a5ucanc <itaycheredman@gmail.com>
2024-04-14 13:58:45 +0300

    bugfix

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d gophi
 docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d evil.gophish.live --register-unsafely-without-email
 
 # Activate https for admin servers
-docker compose exec nginx sed 's/#//g' nginx/conf.d/default.conf
+sudo sed 's/#//g' nginx/conf.d/default.conf
 docker compose exec nginx nginx -s reload
