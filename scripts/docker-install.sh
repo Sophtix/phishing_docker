58800d721ad421b0186a679d0e080292c998b8c5
a5ucanc <itaycheredman@gmail.com>
2024-05-13 12:14:29 +0300

    changed nginx volume to /etc/nginx

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+scripts_dir=$(dirname $0)
+cd $scripts_dir/..
 
 # Build images and create containers
 docker compose up --build -d
 
 # Generate certificats for admin servers
-docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d gophish.live --register-unsafely-without-email
-docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d evil.gophish.live --register-unsafely-without-email
+docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d gophish.co --register-unsafely-without-email
+docker compose run --rm certbot certonly --webroot -w /var/www/certbot/ -d evil.gophish.co --register-unsafely-without-email
 
 # Activate https for admin servers
-sed -i 's/#//g' nginx/conf.d/default.conf
+sed -i 's/#//g' ./nginx/conf.d/default.conf
 docker compose exec nginx nginx -s reload
